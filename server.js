e744ed579fdfd585d3fedb747e9edb868df90464
popKaroum <popkaroum@pop-os.localdomain>
2020-04-14 12:17:57 +0200

    http to https

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const path = require('path');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/dashboard/month', require('./routes/api/dashboard/month'));
+app.use('/api/dashboard/delete', require('./routes/api/dashboard/delete'));
+app.use('/api/dashboard/load', require('./routes/api/dashboard/load'));
+
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
@@ -23,19 +36,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-
-// Connect Database
-connectDB();
-
-// Init Middleware
-app.use(express.json({ extended: false }));
-
-// Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/dashboard/month', require('./routes/api/dashboard/month'));
-app.use('/api/dashboard/delete', require('./routes/api/dashboard/delete'));
-app.use('/api/dashboard/load', require('./routes/api/dashboard/load'));
 
 const PORT = process.env.PORT || 5000;
 
