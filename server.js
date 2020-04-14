4ab9214f302da841deece95cf47f69ebf8c09ffa
popKaroum <popkaroum@pop-os.localdomain>
2020-04-14 12:03:41 +0200

    http to https

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
-    if (process.env.NODE_ENV === ' production ') {
-      if (req.headers.host === ' getcashflow.herokuapp.com ')
-        return res.redirect(301, ' https://www.getcashflow.io ');
-      if (req.headers[' x-forwarded-proto '] !== ' https ')
-        return res.redirect(' https:// ' + req.headers.host + req.url);
+    if (process.env.NODE_ENV === 'production') {
+      if (req.headers.host === 'getcashflow.herokuapp.com')
+        return res.redirect(301, 'https://www.getcashflow.io');
+      if (req.headers['x-forwarded-proto'] !== 'https')
+        return res.redirect('https://' + req.headers.host + req.url);
       else return next();
     } else return next();
   });
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
